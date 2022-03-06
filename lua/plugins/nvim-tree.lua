499e6a68005210227e7cc03e663ac88634108296
nooberfsh <nooberfsh@gmail.com>
2022-03-06 20:59:30 +0800

    modify nvim tree key bindings

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -5,4 +5,7 @@ require'nvim-tree'.setup {
 
 -- 目录后面添加斜杠 '/'
 vim.g.nvim_tree_add_trailing = 1
-vim.keybinds.gmap("n", "<leader>1", "<cmd>NvimTreeToggle<CR>", vim.keybinds.opts)
+
+vim.keybinds.gmap("n", "<A-r>", "<cmd>NvimTreeToggle<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("i", "<A-r>", "<cmd>NvimTreeToggle<CR>", vim.keybinds.opts)
+vim.keybinds.gmap("t", "<A-r>", "<cmd>NvimTreeToggle<CR>", vim.keybinds.opts)
